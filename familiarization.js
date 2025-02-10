4d81e04b1e2aec795430278cf7a37ec59c4d4da5
lorem637 <tariklev51@gmail.com>
2025-02-10 16:07:45 +0200

    перше завдання з git

---
+++ familiarization.js
@@ -0,0 +1,71 @@
+{hello = 'hello';
+console.log(hello);
+}
+{owu = 'owu';
+console.log(owu);}
+{
+    com = 'com';
+    console.log(com);
+}
+{
+    ua = 'ua'
+    console.log(ua);
+}
+{
+    a = 1;
+    console.log(a)
+}
+{
+    b = 10;
+    console.log(b)
+}
+{
+c = -999;
+console.log(c);
+}
+{
+d = 123;
+console.log(d);
+}
+{
+    pi = 3.14;
+    console.log(pi);
+}
+{
+    w = 2.7;
+    console.log(w);
+}
+{
+    f = 16;
+    console.log(f);
+}
+{
+    r = true;
+    console.log(r);
+}
+{
+    R = false;
+    console.log(R);
+}
+/*------------№2-----------*/
+{
+    firstName = 'Левицький';
+    middleName = 'Тарас';
+    lastName = 'Петрович';
+     person = firstName + " " + middleName + " " + lastName;
+    console.log(person);
+}
+
+// let a = 100; let b = '100'; let c = true;
+{
+    let a = 100;
+    console.log(typeof a,a);
+}
+{
+    let b = '100'
+    console.log(typeof b,b);
+}
+{
+    let c = true;
+    console.log(typeof c,c);
+ }
